a754502eb687e60233e19c7262781e0365222c3a
Grandy <45406700@qq.com>
2018-10-29 16:09:34 +0800

    更新文件
    Signed-off-by: Grandy <45406700@qq.com>

---
+++ lianxi/20181029/guess.py
@@ -0,0 +1,17 @@
+import random
+secret=random.randint(1,10)
+print(secret)
+print("猜数字游戏")
+temp=input("不妨猜猜我心中的数字：")
+guess=int(temp)
+while guess!=secret:
+    temp=input('哎呀，猜错了，请重新输入：')4
+    guess=int(temp)
+    if guess==secret:
+        print('猜对了')
+    else:
+        if guess>secret:
+            print('大了，大了')
+        else:
+            print('小了，小了')
+print('游戏结束！')
